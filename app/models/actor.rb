529aee9a586f717bd9037317586ea177d9b3ed7f
Pranik Chainani <pranikc@gmail.com>
2021-06-26 00:07:50 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        self.first_name + " " + self.last_name
+    end
   
+    def list_roles
+        roles_arr = []
+        self.characters.each do |each_char|
+            first = each_char.name
+            last = each_char.show.name
+            complete = first + " - " + last
+            roles_arr << complete
+        end
+        roles_arr
+    end  
 end
