decf65d3015c5415f926e5e43f93097a07ec2fda
Pranik Chainani <pranikc@gmail.com>
2021-06-26 00:07:46 -0400

    commit tree e08b8f6546f1b9f99427555431789e38af06a13d

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        self.actors.map{|actor| actor.full_name}
+    end
+
+    def build_network(arg = {:call_letters => value})
+        network = Network.new
+        self.network = network
+        network.call_letters = arg[:call_letters]
+    end
 end
